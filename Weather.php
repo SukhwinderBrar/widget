ff9691485b857bd035d9e4d761717d4f9eb5a3b5
SukhwinderBrar <57958855+SukhwinderBrar@users.noreply.github.com>
2019-11-21 00:44:05 +0530

    chnage view

--- Weather.php
+++ Weather.php
@@ -13,8 +13,24 @@ class Weather  extends Widget {
         parent::init();
     }
     public function run(){
+
+        $googleApiUrl = "http://api.openweathermap.org/data/2.5/weather?id=" . $cityId . "&lang=en&units=metric&APPID=" . $apiKey;
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $googleApiUrl);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_VERBOSE, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $response = curl_exec($ch);
+
+        curl_close($ch);
+        $data = json_decode($response);
+        $currentTime = time();
         // you can load & return the view or you can return the output variable
-        return $this->render('weather', ['apikey' => $this->apiKey,'cityId'=>$this->cityId]);
+        return $this->render('weather', ['date'=>$data]);
     }
 }
 
